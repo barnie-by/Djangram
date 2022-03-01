845e939a4af15fb58a70296ca71222eb94037d59
vitali_r <rogatsevich.vv@mail.ru>
2022-03-01 14:41:23 +0300

    Slug: Add slug field to Posts Move DB creds into credits.py
    Add credits_example.py

--- blog_core/models.py
+++ blog_core/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from django.urls import reverse
-
 from users.models import CustomUser
 
 
 class Posts(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
-    content = models.TextField(null=True, blank=True, )
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField(null=True, blank=True)
     published = models.DateTimeField(auto_now_add=True)
-    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL',)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL')
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_slug': self.slug})

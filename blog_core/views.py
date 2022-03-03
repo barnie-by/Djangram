8975f57b775de0c389eb09f704a3f421442ecb7f
vitali_r <rogatsevich.vv@mail.ru>
2022-03-03 22:19:00 +0300

    First-view: Created main page with all posts Created possibility to open each post Navigation bar created

--- blog_core/views.py
+++ blog_core/views.py
@@ -1,17 +1,10 @@
-from django.shortcuts import render
 from blog_core.models import Posts
 from django.views.generic import ListView, DetailView
 
 
-# def base(request):
-#     pos = Posts.objects.all()
-#     context = {'pos': pos}
-#     return render(request, 'base.html', context)
-#
-
 class Home(ListView):
     model = Posts
-    template_name = 'base.html'
+    template_name = 'all_posts.html'
     context_object_name = "post"
 
 
